17f8e176ab313229308a1420402ebad7a866eb1d
Kndrzej <kozinskiandrzej4@gmail.com>
2020-07-06 10:43:09 +0200

    Initial commit

---
+++ Assets/Scripts/PlayMusic.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayMusic : MonoBehaviour, IPointerClickHandler
+{
+    private AudioSource audio;
+    private Text buttonDescription;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        buttonDescription = GetComponentInChildren<Text>();
+    }
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+        if (audio.isPlaying == true)
+        {
+            audio.Stop();
+            if (buttonDescription != null)
+            {
+                buttonDescription.text = "Play music";
+            }
+        }
+        else
+        {
+            audio.Play();
+            if (buttonDescription != null)
+            {
+                buttonDescription.text = "Stop music";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TemporaryText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TemporaryText : MonoBehaviour
+{
+    [SerializeField] float displayTime = 3f;
+    void OnEnable()
+    {
+        StartCoroutine(ShowAndHide());
+    }
+    IEnumerator ShowAndHide()
+    {
+        yield return new WaitForSeconds(displayTime);
+        this.gameObject.SetActive(false);
+    }
+}
